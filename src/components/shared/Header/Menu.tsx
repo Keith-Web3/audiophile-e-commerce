91f0c34dd067645f405c020f49bfdd2e2a344c7c
Olorunnishola Olamilekan <olorunnisholaolamilekan@gmail.com>
2023-03-21 12:27:59 +0100

    completed homepage design

---
+++ src/components/shared/Header/Menu.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Products from '../Products'
+
+const Menu: React.FC = function () {
+  return (
+    <div className="menu">
+      <Products />
+    </div>
+  )
+}
+
+export default Menu
