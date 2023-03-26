1d17303000879139a8f137eb13de3c3c83ec42bf
Olorunnishola Olamilekan <olorunnisholaolamilekan@gmail.com>
2023-03-26 07:08:23 +0100

    fixed mobile cart overflow

---
+++ src/components/pages/Checkout/Checkout.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Checkout: React.FC = function () {
+  return (
+    <div className="checkout">
+      <h1>Checkout</h1>
+      <div className="billing-details">
+        <h2>billing details</h2>
+      </div>
+    </div>
+  )
+}
+
+export default Checkout
